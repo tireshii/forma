68c411661cb26912904f4a0923230dcbd69623bd
tireshit <egor.trusakov@gmail.com>
2023-07-09 19:01:04 +0300

    forsen

---
+++ main.js
@@ -0,0 +1,38 @@
+console.log("ты долбаеб")
+let form = document.getElementById("form")
+addEventListener('submit', (event) => {});
+onsubmit = (event) => {
+    let nomer = document.getElementById("nomer")
+    let date = document.getElementById("date")
+    let name = document.getElementById("name")
+    let cost = document.getElementById("cost")
+    let cent = document.getElementById("cent")
+    let coef = document.getElementById("coef")
+    event.preventDefault()
+    console.log(JSON.stringify({
+                input_title: nomer,
+                input_data: date,
+                input_pred: name,
+                input_cost: cost,
+                input_cent: cent,
+                input_cost_coef: coef
+            }))
+    const post = async () => {
+        const response = await fetch( ``, {
+            method: `POST`,
+            headers ,
+            body: JSON.stringify({
+                input_title: nomer,
+                input_data: date,
+                input_pred: name,
+                input_cost: cost,
+                input_cent: cent,
+                input_cost_coef: coef
+            })
+        })
+        const data = await response.json()
+        if(data.succes){
+            console.log("Пошел нахуй")
+        }
+    }
+}
