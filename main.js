b32e0b55c711b54a39c18ae627de67af4e9cdef2
tireshit <egor.trusakov@gmail.com>
2023-07-09 19:19:39 +0300

    MOX

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 let form = document.getElementById("form")
 addEventListener('submit', (event) => {});
 onsubmit = (event) => {
+    //really dont like this 
     let nomer = document.getElementById("nomer")
     let date = document.getElementById("date")
     let name = document.getElementById("name")
     let cost = document.getElementById("cost")
     let cent = document.getElementById("cent")
     let coef = document.getElementById("coef")
+    
     event.preventDefault()
-    console.log(JSON.stringify({
-                input_title: nomer,
-                input_data: date,
-                input_pred: name,
-                input_cost: cost,
-                input_cent: cent,
-                input_cost_coef: coef
-            }))
     const post = async () => {
         const response = await fetch( ``, {
             method: `POST`,
@@ -34,3 +28,7 @@ onsubmit = (event) => {
         }
     }
 }
+/*Мох! Стать похожим меньше на цветок, больше на
+Мох! Японский сад промок, воду пьёт зелёный мох
+Неприметным ковром стелется в тени цветов, репней и пестиков, шипов
+Мы все исчезнем, но не мох*/ 
